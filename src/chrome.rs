ffc91c377855966ab52356c345080ff01d772c89
rherv <137662855+rherv@users.noreply.github.com>
2023-11-27 19:45:23 -0800

    Not compiling

---
+++ src/chrome.rs
@@ -0,0 +1,7 @@
+use std::path::PathBuf;
+use dirs;
+
+const CACHE_DIR: PathBuf = dirs::cache_dir().unwrap();
+
+pub fn test() {
+}
--- src/util.rs
+++ src/util.rs
@@ -1,21 +1,29 @@
-use std::io;
+use std::{fs, io};
 use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
-pub fn get_files_recursive(dir_path: PathBuf) -> io::Result<Vec<PathBuf>> {
+pub fn get_files_recursive(dir_path: impl AsRef<Path>) -> io::Result<Vec<PathBuf>> {
     let mut file_paths: Vec<PathBuf> = Vec::new();
     for entry in WalkDir::new(dir_path).follow_links(true) {
-        match entry {
-            Ok(entry) => {
-                if entry.file_type().is_file() {
-                    if let Some(path) = entry.path().to_path_buf() {
-                        file_paths.push(path);
-                    }
-                }
-            }
-            Err(e) => return Err(io::Error::from(e)),
+        let entry = entry?;
+        if entry.file_type().is_file() {
+            file_paths.push(entry.path().to_path_buf());
         }
     }
 
     Ok(file_paths)
+}
+
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in WalkDir::new(src).follow_links(true) {
+        let entry = entry?;
+        if entry.file_type().is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+
+    Ok(())
 }
